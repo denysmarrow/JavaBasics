36a5a4f49aacd8725d0816d6ee013398d44c74df
denysmarrow <mzmarrowz@gmail.com>
2022-01-03 20:34:10 -0500

    1 out of 3

--- src/com/careerdevs/Main.java
+++ src/com/careerdevs/Main.java
@@ -3,6 +3,11 @@ package com.careerdevs;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        var firstName = "Denys";
+        var lastName = "Marrow";
+        var fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+
+
     }
 }
