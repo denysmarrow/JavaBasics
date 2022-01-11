a6bfbfc676d50bca114049e454de07f0f40c5bba
denysmarrow <mzmarrowz@gmail.com>
2022-01-11 18:37:53 -0500

    2 out of 3

---
+++ src/com/careerdevs/javabasicschallenge1/CarCollector.java
@@ -0,0 +1,19 @@
+package com.careerdevs.javabasicschallenge1;
+
+public class CarCollector {
+//    public static void main(String[] args) {
+        public static String make = " ";
+        public String model = "";
+        public int mileage = 0;
+        public float gasTank = 00000000;
+
+        public CarCollector (String make, String model, int mileage, float gasTank){
+                this.make = make;
+                this.model = model;
+                this.mileage = mileage;
+                this.gasTank = gasTank;
+
+    }
+
+
+}
---
+++ src/com/careerdevs/javabasicschallenge1/Main.java
@@ -0,0 +1,13 @@
+package com.careerdevs.javabasicschallenge1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        CarCollector myCarCollector = new CarCollector("Camaro", "Chevy", 100000, 0.10F);
+
+        System.out.println(CarCollector.make);
+        System.out.println(CarCollector.model);
+
+    }
+}
+
