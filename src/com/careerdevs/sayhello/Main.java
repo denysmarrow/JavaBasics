6d8bb667fdf8bf449d8215fac1358a25872d9240
denysmarrow <mzmarrowz@gmail.com>
2022-01-13 17:29:33 -0500

    refactoring

--- src/com/careerdevs/sayhello/Main.java
+++ src/com/careerdevs/sayhello/Main.java
@@ -4,11 +4,7 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner whatIsYourName = new Scanner(System.in);
 
-        System.out.print("Please enter your\nName: ");
-        String name = whatIsYourName.nextLine();
-        System.out.println("Hello, nice to meet you " + name);
 
         Scanner whatIsYourFullName = new Scanner(System.in);
         System.out.println("What is your first\nName: ");
@@ -17,6 +13,14 @@ public class Main {
         String lastName = whatIsYourFullName.nextLine();
 
         System.out.println("It is so nice to formally meet you " + firstName + " " + lastName );
+    }
+
+    public static void whatisYourName() {
+        Scanner whatIsYourName = new Scanner(System.in);
+
+        System.out.print("Please enter your\nName: ");
+        String name = whatIsYourName.nextLine();
+        System.out.println("Hello, nice to meet you " + name);
     }
 
 
