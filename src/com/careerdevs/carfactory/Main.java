c645a08be2dab340aa58af8a0181b288703af246
denysmarrow <mzmarrowz@gmail.com>
2022-01-14 19:17:02 -0500

    new package

---
+++ src/com/careerdevs/carfactory/Main.java
@@ -0,0 +1,19 @@
+package com.careerdevs.carfactory;
+
+import java.util.Scanner;
+
+public class Main {
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+            createACar();
+
+
+
+    }
+
+        public static String createACar(){
+
+        String ownerOfCar = getUserInput("");
+    }
+}
